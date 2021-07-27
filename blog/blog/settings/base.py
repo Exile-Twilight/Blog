7b734cbad8466ccc402ba248a0ccc27bae609dfe
Kenny Lee <xlee.dell@outlook.com>
2021-07-27 12:48:06 +0800

    增加项目Model

--- blog/blog/settings/base.py
+++ blog/blog/settings/base.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app_blog',
+    'app_config',
+    'app_comment',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
